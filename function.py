f60022724dc784f2f9855a4c5f2926dcdd2033ed
Rashid Iqbal <116454295+rashidiqbal08@users.noreply.github.com>
2023-08-26 01:46:09 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,121 +1,121 @@
-##FUNCTION
+#FUNCTION
 
-#In python, the function definition should always be present before the function call.
+# In python, the function definition should always be present before the function call.
 #  Otherwise, we will get an error.
 
-#DOCSTRING-It is briefly used to explain what a function does.
+# DOCSTRING-It is briefly used to explain what a function does.
 
-# def hello(name):
-#     """this will return hello with name"""
-#     return "hello " +name+" good morning"
+def hello(name):
+    """this will return hello with name"""
+    return "hello " +name+" good morning"
    
 
-# print(hello("Rashid"))
+print(hello("Rashid"))
 
-# def per(a,b,c):
-#     """this function will calculate the
-#     percentage of student"""
-#     print("Total attendance of {} are".format(c) ,(b/a)*100)
-
-
-# a=int(input("Enter the total no of days."))
-# b=int(input("Enter the no of days present"))
-# per(a,b,"Rashid")
+def per(a,b,c):
+    """this function will calculate the
+    percentage of student"""
+    print("Total attendance of {} are".format(c) ,(b/a)*100)
 
 
-# #We can also use augument as key value pair(order does not matter.)
-
-# def aiml_sub(a,b,c):
-#     print("The subjects of aiml branch: ",a,b,c)
-
-# aiml_sub(c="artificial intelligence", a="machine learning", b="Data science.")
+a=int(input("Enter the total no of days."))
+b=int(input("Enter the no of days present"))
+per(a,b,"Rashid")
 
 
-##return vs print in function
-#- if we're using return then we have to use print with call
-#- but if we use print inside func then we'll get output after just calling
+#We can also use augument as key value pair(order does not matter.)
 
-# def add(a,b):
-#     return a+b
+def aiml_sub(a,b,c):
+    print("The subjects of aiml branch: ",a,b,c)
 
-# print(add(5,3))
-
-# def add2(a,b):
-#     print("The sum is: ",a+b)
-
-# add2(5,6)
+aiml_sub(c="artificial intelligence", a="machine learning", b="Data science.")
 
 
-#fuction to print the last char of string
-# def last(name):
-#     return name[-1]
+# #return vs print in function
+# - if we're using return then we have to use print with call
+# - but if we use print inside func then we'll get output after just calling
 
-# print(last("Rashid"))
+def add(a,b):
+    return a+b
 
-# #func for odd even
+print(add(5,3))
 
-# def odd_even(num):
-#     if num%2==0:
-#         return True  #return "even"
-#     else:
-#         return False    #return "odd"
+def add2(a,b):
+    print("The sum is: ",a+b)
 
-# print(odd_even(14)) 
-
-# #this will just give true or false
-# def is_even(num):
-#     return num%2==0
-
-# print(is_even(13))
+add2(5,6)
 
 
-#func to print the greatest of three
+fuction to print the last char of string
+def last(name):
+    return name[-1]
 
-# def great(a,b,c):
-#     if a>b:
-#         if a>c:
-#             return a, "is greatest"
-#         return c, 'is greatest'
-#     else:
-#         if b>c:
-#             return b, "is greatest"
-#         return c, 'is greatest'
+print(last("Rashid"))
 
-# print(great(45, 55,8)) 
+#func for odd even
 
+def odd_even(num):
+    if num%2==0:
+        return True  #return "even"
+    else:
+        return False    #return "odd"
 
-##function for palindrome
+print(odd_even(14)) 
 
-# def pal(string):
-#     x=string[::-1]
-#     if x==string:
-#         return True
-#     return False
+#this will just give true or false
+def is_even(num):
+    return num%2==0
 
-# print(pal("madam"))
+print(is_even(13))
 
 
-##check int is palindrom or not
+# func to print the greatest of three
+
+def great(a,b,c):
+    if a>b:
+        if a>c:
+            return a, "is greatest"
+        return c, 'is greatest'
+    else:
+        if b>c:
+            return b, "is greatest"
+        return c, 'is greatest'
+
+print(great(45, 55,8)) 
 
 
-# def pal_num(num):
-#     a=num
+#function for palindrome
 
-#     #code to reversed an integer
-#     store=0
-#     while a!=0:
-#         r= a%10
-#         store= (store*10) +r
-#         a= a//10
-#     print("reversed no", store)
-#     print("original int", num)
+def pal(string):
+    x=string[::-1]
+    if x==string:
+        return True
+    return False
 
-#     #comparing the original value and reversed one.
-#     if num==store:
-#         return True
-#     return False
+print(pal("madam"))
+
+
+#check int is palindrom or not
+
+
+def pal_num(num):
+    a=num
+
+    #code to reversed an integer
+    store=0
+    while a!=0:
+        r= a%10
+        store= (store*10) +r
+        a= a//10
+    print("reversed no", store)
+    print("original int", num)
+
+    #comparing the original value and reversed one.
+    if num==store:
+        return True
+    return False
     
 
-# print(pal_num(232))
+print(pal_num(232))
 
 
