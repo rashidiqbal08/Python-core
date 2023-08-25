2ecb7ee7f5bbc136d0b22b51408bec76ebbb4ba2
Rashid Iqbal <116454295+rashidiqbal08@users.noreply.github.com>
2023-08-26 01:44:16 +0530

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -1,27 +1,27 @@
-# i= input(("What is your name\n: "))
-# print("Hello, ",i)
+i= input(("What is your name\n: "))
+print("Hello, ",i)
 
-##typecasting the input to string
+#typecasting the input to string
 
-# a=str(input("enter the no:  "))
-# print(a)
-# print(type(a))
+a=str(input("enter the no:  "))
+print(a)
+print(type(a))
 
-#typecasting input to integer
+typecasting input to integer
 
-# b=int(input("Enter the no: "))
-# print(b)
-# print(type(b))
+b=int(input("Enter the no: "))
+print(b)
+print(type(b))
 
 
-#taking multple inputs
+taking multple inputs
 
-# x,y=input("x: "),input("y: ")
-# print(x,""+y)
+x,y=input("x: "),input("y: ")
+print(x,""+y)
 
-#another method  .split()
+another method  .split()
 
-# a,b,c=input('x').split()   #we'll use space while giving input
-# print(a,b,c)
+a,b,c=input('x').split()   #we'll use space while giving input
+print(a,b,c)
 
 
